4b4292a97837758d8f7bbeb424ff00de271d9d26
bintangpz <mc.bintang@gmail.com>
2022-10-21 16:55:05 +0800

    modified:   lib/login_page.dart

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -11,22 +11,23 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromARGB(255, 35, 217, 15),
         body: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20),
-            child: Column(children: <Widget>[
-              TextFormField(
-                decoration: InputDecoration(hintText: "Masukkan Email"),
+              Container(
+                padding: EdgeInsets.all(20),
+                child: Column(children: <Widget>[
+                  TextFormField(
+                    decoration: InputDecoration(hintText: "Masukkan Email"),
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(hintText: "Masukkan Password"),
+                  ),
+                  ElevatedButton(onPressed: () {}, child: Text("LOGIN")),
+                ]),
               ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(hintText: "Masukkan Password"),
-              ),
-              ElevatedButton(onPressed: () {}, child: Text("LOGIN")),
-            ]),
-          ),
-        ]));
+            ]));
   }
 }
