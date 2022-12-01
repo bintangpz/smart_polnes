134baee923e28f7e3c14bf46510399e662989275
bintangpz <mc.bintang@gmail.com>
2022-12-01 17:51:21 +0800

    deleted:    lib/home.dart 	deleted:    lib/login_page.dart 	modified:   lib/main.dart 	deleted:    lib/splashscreen.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:smart_polnes/splashscreen.dart';
 
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    title: 'SELAMAT DATANG',
-    home: SplashScreenPage(),
-  ));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SMART TI POLNES',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Selamat Datang'),
+        ),
+        body: Center(
+          child: Text('hallo'),
+        ),
+      ),
+    );
+  }
 }
