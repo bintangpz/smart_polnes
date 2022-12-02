e1ab4c0e3f71fc37eebda343b27d799e1e07e612
bintangpz <mc.bintang@gmail.com>
2022-12-02 16:08:30 +0800

    new file:   lib/color_intro.dart 	modified:   lib/intro_model.dart 	modified:   lib/intro_screen.dart 	modified:   pubspec.yaml

--- lib/intro_model.dart
+++ lib/intro_model.dart
@@ -1,24 +1,7 @@
-class IntroModel {
-  final String image;
-  final String title;
-  final String description;
+class Intro {
+  String image;
+  String title;
+  String description;
 
-  IntroModel(
-      {required this.image, required this.title, required this.description});
+  Intro({this.image, this.title, this.description});
 }
-
-final List<IntroModel> intromodel_data = [
-  IntroModel(
-      image: "images/logo_polnes.png",
-      title: "Selamat Datang",
-      description: "Selamat Datang di Aplikasi SMART TI POLNES"),
-  IntroModel(
-      image: "images/logo_ti.png",
-      title: "Aplikasi Smart TI",
-      description:
-          "Aplikasi ini digunakan untuk memudahkan\nsegala kebutuhan terkait Jurusan TI"),
-  IntroModel(
-      image: "images/logo_enjoy.jpg",
-      title: "Enjoy",
-      description: "Selamat Menikmati"),
-];
