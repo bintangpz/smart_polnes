ebdc19d8a872e75cd715cdfdb053b2388ff271cf
bintangpz <mc.bintang@gmail.com>
2022-12-05 15:27:19 +0800

    modified:   lib/body.dart 	new file:   lib/constants.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/body.dart
+++ lib/body.dart
@@ -10,11 +10,79 @@ class Body extends StatelessWidget {
     return SingleChildScrollView(
       child: Column(
         children: <Widget>[
-          HeaderWithSearchBox(size: size)
-      ],
-    ),
+          HeaderWithSearchBox(size: size),
+          TitleWithMoreBtn(
+            title: "Recommended",
+            press: () {},
+          ),
+        ],
+      ),
     );
-}
+  }
 }
 
+class TitleWithMoreBtn extends StatelessWidget {
+  const TitleWithMoreBtn({
+    Key? key,
+    required this.title,
+    required this.press,
+  }) : super(key: key);
+  final String title;
+  final Function press;
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      child: Row(children: <Widget>[
+        TitleWithCustomUnderLine(text: title),
+        Spacer(),
+        TextButton(
+            style: ButtonStyle(
+                shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+              side: BorderSide(color: Color.fromARGB(255, 6, 71, 140)),
+            ))),
+            onPressed: () {},
+            child: Text(
+              "More",
+              style: TextStyle(color: Color.fromARGB(255, 7, 64, 156)),
+            ))
+      ]),
+    );
+  }
+}
+
+class TitleWithCustomUnderLine extends StatelessWidget {
+  const TitleWithCustomUnderLine({
+    Key? key,
+    required this.text,
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 24,
+        child: Stack(children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: Text(
+              text,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              margin: EdgeInsets.only(right: 0.8),
+              height: 7,
+              color: Colors.blue.withOpacity(0.2),
+            ),
+          )
+        ]));
+  }
+}
