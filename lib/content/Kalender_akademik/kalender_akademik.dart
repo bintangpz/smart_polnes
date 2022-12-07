b0edd491eb88eed0c09371353df7fc2d1900e405
bintangpz <mc.bintang@gmail.com>
2022-12-07 15:43:15 +0800

    modified:   lib/content/Kalender_akademik/kalender_akademik.dart

--- lib/content/Kalender_akademik/kalender_akademik.dart
+++ lib/content/Kalender_akademik/kalender_akademik.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:smart_polnes/content/Kalender_akademik/body.dart';
-import 'package:smart_polnes/mainpage/My_bottom_navigation_bar.dart';
 
 class KalenderAkademik extends StatelessWidget {
   const KalenderAkademik({super.key});
