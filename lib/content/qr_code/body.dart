0a5852070687e66844e9d76d1ac35575d15604b8
bintangpz <mc.bintang@gmail.com>
2022-12-13 00:19:25 +0800

    modified:   lib/content/qr_code/body.dart

--- lib/content/qr_code/body.dart
+++ lib/content/qr_code/body.dart
@@ -1,7 +1,4 @@
 import 'dart:developer';
-import 'dart:io';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
@@ -30,11 +27,7 @@ class _BodyQRScanState extends State<BodyQRScan> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: <Widget>[
-                  if (result != null)
-                    Text(
-                        'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
-                  else
-                    const Text('Scan a code'),
+                  if (result != null) Text('Data: ${result!.code}')
                 ],
               ),
             ),
