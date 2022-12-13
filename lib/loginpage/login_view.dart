aed7162b9b7050986218a7298373fed6e30cde2a
bintangpz <mc.bintang@gmail.com>
2022-12-13 18:45:51 +0800

    modified:   lib/loginpage/login_view.dart

--- lib/loginpage/login_view.dart
+++ lib/loginpage/login_view.dart
@@ -1,4 +1,3 @@
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_login/flutter_login.dart';
@@ -9,10 +8,10 @@ const users = const {
   'mc.bintang@gmail.com': '12345',
 };
 
-
 class LoginScreen extends StatelessWidget {
-  
   Duration get loginTime => Duration(milliseconds: 1000);
+
+  FirebaseAuth auth = FirebaseAuth.instance;
 
   Future<String?> _authUser(LoginData data) {
     debugPrint('Name: ${data.name}, Password: ${data.password}');
@@ -27,13 +26,28 @@ class LoginScreen extends StatelessWidget {
     });
   }
 
-
-  Future<String?> _signupUser(SignupData data) {
-    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return null;
-    });
+  Future<String?> DaftarViaEmail(SignupData data) async {
+    try {
+      await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: data.name.toString(),
+        password: data.password.toString(),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
   }
+  // Future<String?> _signupUser(SignupData data) {
+  //   debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
+  //   return Future.delayed(loginTime).then((_) {
+  //     return null;
+  //   });
+  // }
 
   Future<String> _recoverPassword(String name) {
     debugPrint('Name: $name');
@@ -51,7 +65,7 @@ class LoginScreen extends StatelessWidget {
       title: 'SMART TI',
       logo: AssetImage('images/logo_ti.png'),
       onLogin: _authUser,
-      onSignup: _signupUser,
+      onSignup: DaftarViaEmail,
       onSubmitAnimationCompleted: () {
         Navigator.of(context).pushReplacement(MaterialPageRoute(
           builder: (context) => Dashboard(),
