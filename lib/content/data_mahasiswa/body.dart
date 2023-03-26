19bfdbfb9ee23906ed8e13f63d5ab01bd9f2c6d7
bintangpz <mc.bintang@gmail.com>
2023-03-27 01:17:47 +0800

    modified:   lib/content/data_mahasiswa/body.dart

--- lib/content/data_mahasiswa/body.dart
+++ lib/content/data_mahasiswa/body.dart
@@ -2,8 +2,7 @@ import 'package:d_chart/d_chart.dart';
 import 'package:flutter/material.dart';
 
 class Body extends StatelessWidget {
-  const Body({required Key? key});
-  
+  const Body({required Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
