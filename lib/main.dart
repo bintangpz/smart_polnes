32bed91f153de1cdd8fc144ac629e70afcf5c5a6
bintangpz <mc.bintang@gmail.com>
2023-03-28 23:19:36 +0800

    modified:   lib/main.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+
 import 'package:smart_polnes/loginpage/login_view.dart';
 import 'package:introduction_screen/introduction_screen.dart';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
-
-Future <void> main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform,);
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const App());
 }
-
 
 class App extends StatelessWidget {
   const App({super.key});
